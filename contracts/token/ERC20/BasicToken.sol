12c35461aed1276632a3b5f838e950e0ec0b38c1
Thanyapat Kosiumnuay <thanyapat.kosi@gmail.com>
2018-01-29 14:07:25 +0000

    Update BasicToken.sol

--- contracts/token/ERC20/BasicToken.sol
+++ contracts/token/ERC20/BasicToken.sol
@@ -4,30 +4,16 @@ pragma solidity ^0.4.18;
 import "./ERC20Basic.sol";
 import "../../math/SafeMath.sol";
 
-
-/**
- * @title Basic token
- * @dev Basic version of StandardToken, with no allowances.
- */
 contract BasicToken is ERC20Basic {
   using SafeMath for uint256;
 
   mapping(address => uint256) balances;
-
   uint256 totalSupply_;
 
-  /**
-  * @dev total number of tokens in existence
-  */
   function totalSupply() public view returns (uint256) {
     return totalSupply_;
   }
-
-  /**
-  * @dev transfer token for a specified address
-  * @param _to The address to transfer to.
-  * @param _value The amount to be transferred.
-  */
+  
   function transfer(address _to, uint256 _value) public returns (bool) {
     require(_to != address(0));
     require(_value <= balances[msg.sender]);
@@ -38,12 +24,7 @@ contract BasicToken is ERC20Basic {
     Transfer(msg.sender, _to, _value);
     return true;
   }
-
-  /**
-  * @dev Gets the balance of the specified address.
-  * @param _owner The address to query the the balance of.
-  * @return An uint256 representing the amount owned by the passed address.
-  */
+  
   function balanceOf(address _owner) public view returns (uint256 balance) {
     return balances[_owner];
   }
